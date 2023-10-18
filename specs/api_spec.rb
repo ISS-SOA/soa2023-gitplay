62bd22127e20f6de0569a79c99eadcf47c33e9fd
Soumya Ray <soumya.ray@gmail.com>
2023-10-18 20:17:54 +0800

    Comments for tests

--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -3,6 +3,7 @@
 require_relative 'spec_helper'
 require_relative 'lib/api_explore'
 
+# This is a test for the APIExplore::Client class
 describe 'API' do
   it 'should return 200' do
     _(APIExplore::Client.hello).must_equal 'hello'
